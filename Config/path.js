96e4e6d84f7860235c8b5a47c4177a0546ed5c4b
Sarath Babu Vajiravelu Anbalagan <jcnt514@macbook20.jbhunt.com>
2018-04-30 09:48:27 -0500

    30 commit

--- Config/path.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'react-native';
-
-const path = require('path');
-
-process.env.ipaPath = (path.relative('/Users/jisqsk3/Desktop', '/Carrier.ipa'));
-
---
+++ app_mobile_carrier360/__mocks__/react-native-config.js
@@ -0,0 +1,4 @@
+export default {
+  APP_NAME: 'carrier360',
+  FORGOT_PASSWORD_URL: 'https://mobilecarrier-tst.jbhunt.com/ws_mobile_carrier_gateway/ws_security_mobile_carrier360UserAuth/userauth/password/forgot/{email}/',
+};
